57a12cf69f7a4cdfdb74d44004423b6e42b806fb
dgrodriguez24 <dgrodriguez24@hotmail.com>
2023-04-28 12:42:56 -0400

    generics: types of generic functions

--- generics.ts
+++ generics.ts
@@ -161,3 +161,134 @@ function loggingIdentity<Type>(arg: Array<Type>): Array<Type> {
     languages. In the next section, we'll cover how you can create your
     own generic types like Array<Type>.
 */
+
+/*
+    Generic Types
+
+    Exploring the types of functions themselves. In the examples below 
+    we wille explore the types of generic functions themselves.
+
+    In previous sections, we created generic identity functions 
+    that worked over a range of types. In this section, we'll explore 
+    the type of the functions themselves and how to create generic interfaces.
+
+    The type of generic functions is just like those non-generic functions, with the 
+    type parameters listed first, similarly to function declarations:
+
+        function identity<Type>(arg: Type): Type {
+            return arg;
+        }
+
+        let myIdentity: <Type>(arg: Type) => Type = identity;
+
+
+    We could have also used a different name for the generic type parameter 
+    in the type, so long as the number of type variables and how the type 
+    variables are used line up.
+
+        function identity<Type>(arg: Type): Type {
+            return arg;
+        }
+
+        let myIdentity: <Input>(arg: Input) => Input = identity;
+
+
+    We can also write the generic type as a call signature of an object literal type:
+
+        function identity<Type>(arg: Type): Type {
+            return arg;
+        }
+
+        let myIdentity: { <Type>(arg: Type): Type } = identity;
+
+
+    Which leads us to writing our first generic interface. Let's make the object 
+    literal from the previous example and move it to an interface:
+*/
+
+interface GenericIdentityFn {
+    <Type>(arg: Type): Type;
+}
+
+function identity<Type>(arg: Type): Type {
+    return arg;
+}
+
+let myIdentity: GenericIdentityFn = identity;
+
+/*
+    Generic Types Cont'd
+
+    In a similar example, we may want to move the generic parameter to be 
+    a parameter of the whole interface. This lets us see what type(s) we're
+    generic over (e.g., Dictionary<string> rather than just Dictionary). 
+    
+    This makes the type parameter visible to all the other members of the interface.
+*/
+
+interface GenericIdentityFnTwo<Type> {
+    (arg: Type): Type;
+}
+
+function identityTwo<Type>(arg: Type): Type {
+    return arg;
+}
+
+let myIdentityTwo: GenericIdentityFnTwo<number> = identity;
+
+/*
+    Generic Types Cont'd
+
+    Notice that our example has changed to be something slightly different.
+    Instead of describing a generic function, we now have a non-generic function 
+    signature that is a part of a generic type. 
+
+    When we use `GenericIdentityFnTwo`, we now will also need to specify the 
+    corresponding type argument (here: `number`), effectively locking in what the 
+    underlying call signature will use.
+
+    Understanding when to put the type parameter directly on the call signature 
+    and when to put it on the interface itself will be helpful in describing what aspects of 
+    a type are generic.
+
+    In addition to generic interfaces, we can also create generic classes. Note 
+    that it is not possible to create generic enums and namespaces.
+
+    
+    Q & A
+
+    Why am I able to assign the identity() function to myIdentity, if 
+    myIdentity is looking for a function that accepts a number as the argument 
+    and returns a number?
+
+        interface GenericIdentityFn<Type> {
+        (arg: Type): Type;
+        }
+        
+        function identity<Type>(arg: Type): Type {
+        return arg;
+        }
+        
+        let myIdentity: GenericIdentityFn<number> = identity;
+
+
+    You are able to assign the identity function `identity<Type>` to `myIdentity` 
+    because `identity` is a function that accepts any type of argument and returns 
+    the same type of argument, including a number.
+
+    In this case, the `GenericIdentityFn` interface is a generic interface 
+    that describes a function type that accepts an argument of a specified 
+    type `Type` and returns a value of the same type. When you declare `myIdentity`
+    as a variable of type `GenericIdentityFn<number>`, you are specifying that it is 
+    a function that takes a number argument and returns a number value.
+
+    Since the `identity` function also confirms to the `GenericFunctionFn` 
+    interface and returns a value of the same type as its argument, it can 
+    be assigned to `myIdentity`. 
+
+    The generic type parameter `Type` of the `identity` function is inferred to 
+    be `number` based on the type of `myIdentity`. '
+
+    Therefore, `myIdentity` can be used as a function that takes a number 
+    argument and returns a number value, just like the `identity` function.
+*/
